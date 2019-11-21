e5348d2fe2a9326fb8991f9cb0a22a445ac2262b
Seth Collins <sethcollins@gmail.com>
2019-11-21 07:55:24 -0600

    [components] Split app into multiple components

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div className="hero-container">
+    <div className="hero">
+    <h1> Hello There.</h1>
+    </div>
+    </div>
+    
+  );
+}
+
+export default Hero;
